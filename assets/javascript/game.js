25121424430bc6c903c691a746c51ac9f061eec1
unknown <crawford7100@gmail.com>
2018-02-12 22:36:13 -0500

    backing up

---
+++ assets/javascript/game.js
@@ -0,0 +1,100 @@
+// Random # Generator //
+$( document ).ready(function(){
+    var Random=Math.floor(Math.random()*101+19)
+
+$('#randomNumber').text(Random);
+
+// Assigning crystals a random value between 1-12 //
+var num1= Math.floor(Math.random()*11+1)
+var num2= Math.floor(Math.random()*11+1)
+var num3= Math.floor(Math.random()*11+1)
+var num4= Math.floor(Math.random()*11+1)
+
+// Setting Variables //
+var userTotal= 0; 
+var wins= 0;
+var losses = 0;
+  
+$('#numberWins').text(wins);
+$('#numberLosses').text(losses);
+
+// Game Reset //
+function reset(){
+    Random=Math.floor(Math.random()*101+19);
+    console.log(Random)
+    $('#randomNumber').text(Random);
+    num1= Math.floor(Math.random()*11+1);
+    num2= Math.floor(Math.random()*11+1);
+    num3= Math.floor(Math.random()*11+1);
+    num4= Math.floor(Math.random()*11+1);
+    userTotal= 0;
+    $('#finalTotal').text(userTotal);
+} 
+
+// Wins //
+function yay(){
+alert("You won!");
+wins++; 
+$('#numberWins').text(wins);
+reset();
+}
+
+// Losses //
+function loser(){
+alert ("You lose!");
+losses++;
+$('#numberLosses').text(losses);
+reset()
+}
+
+  
+$('#one').on ('click', function(){
+      userTotal = userTotal + num1;
+      console.log("New userTotal= " + userTotal);
+      $('#finalTotal').text(userTotal); 
+            //sets win/lose conditions
+          if (userTotal == Random){
+            yay();
+          }
+          else if ( userTotal > Random){
+            loser();
+          }   
+    })  
+    $('#two').on ('click', function(){
+      userTotal = userTotal + num2;
+      console.log("New userTotal= " + userTotal);
+      $('#finalTotal').text(userTotal); 
+          if (userTotal == Random){
+            yay();
+          }
+          else if ( userTotal > Random){
+            loser();
+          } 
+    })  
+    $('#three').on ('click', function(){
+      userTotal = userTotal + num3;
+      console.log("New userTotal= " + userTotal);
+      $('#finalTotal').text(userTotal);
+
+
+// Win Conditions //      
+            if (userTotal == Random){
+            yay();
+          }
+          else if ( userTotal > Random){
+            loser();
+          } 
+    })  
+    $('#four').on ('click', function(){
+      userTotal = userTotal + num4;
+      console.log("New userTotal= " + userTotal);
+      $('#finalTotal').text(userTotal); 
+        
+            if (userTotal == Random){
+            yay();
+          }
+          else if ( userTotal > Random){
+            loser();
+          }
+    });   
+  }); 
